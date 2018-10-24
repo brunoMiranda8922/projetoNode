b5c5e9463c2c8686cba8d209cfd09b3bdf081275
Bruno <bruno.silva@catho.com>
2018-10-24 18:01:04 -0300

    refatoração nas funções

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 var express = require("express");
 var consign = require("consign");
 
-const port = 3000;
 const app = express();
 
-app.set("json spaces", 4);
-
 consign()
-  .include("./routes")
+  .include("./models")
+  .then("./lib/middleware.js")
+  .then("./routes")
+  .then("./lib/boot.js")
   .into(app);
 
-app.listen(port, () => { 
-    console.log(`server up - porta ${port}`);
-});
