480e457aa4a71d4d23775c1c44fc0193170673a7
Bruno <bruno.silva@catho.com>
2018-10-24 18:20:48 -0300

    arquivo de configuração do database

---
+++ models/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  database: "node",
+  username: "",
+  password: "",
+  params: {
+    dialect: "sqlite",
+    storage: "node.sqlite",
+    define: {
+      underscored: true
+    }
+  }
+}; 
