fab284255a58bcd0dac698537d6ef36e0c1fd2c7
Bruno <bruno.silva@catho.com>
2018-10-26 19:29:29 -0300

    definindo estrutra de dados

---
+++ db.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+const config = require('./models/config.js');
+let sequelize = null;
+
+module.exports = () => { 
+  if (!sequelize) {
+    sequelize = new Sequelize(
+      config.database,
+      config.username,
+      config.password,
+      config.params
+    );
+  }
+  return sequelize;
+};
