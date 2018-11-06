4db001ef46c3e1700a5a917debac46187e411d97
Bruno <bruno.silva@catho.com>
2018-11-06 18:55:25 -0200

    ajustando chamada do consign

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var consign = require('consign');
 const app = express();
 
 consign()
-  .include('db.js')
-  .then('./models')
-  .then('./lib/middleware.js')
-  .then('./routes')
-  .then('./lib/boot.js')
+  .include('lib/config.js')
+  .then('db.js')
+  .then('lib/middleware.js')
+  .then('routes')
+  .then('lib/boot.js')
   .into(app);
