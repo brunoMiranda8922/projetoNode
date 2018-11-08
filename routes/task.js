bdae2cbafc9ab0b9d67437a1379681459a2bb36e
Bruno <bruno.silva@catho.com>
2018-11-08 13:58:11 -0200

    implementando CRUD nasrotas das tasks

--- routes/task.js
+++ routes/task.js
@@ -1,9 +1,58 @@
 module.exports = app => {
-  const Tasks = app.db.models.task;
-  app.get('/tarefas', (req, res) => {
-    Tasks.findAll({}, (task) => {
-      res.send({task});
-      res.status(200);
+  const Tasks = app.db.models.Tasks;
+
+  app.route('/tasks')
+    .get((req, res) => {
+      // "tasks": lista tarefas
+      Tasks.findAll({})
+        .then(result => res.send(result))
+        .catch(error => {
+          res.status(412).send({msg: error.message});
+        });
+    })
+    .post((req, res) => {
+      // "tasks": Cadastra uma nova tarefa
+      Tasks.create(req.body)
+        .then(result => res.json(result).status(204))
+        .catch(error => {
+          res.status(412).json({msg: error.message});
+        });
     });
-  });
+  app.route('/tasks/:id')
+    .get((req, res) => {
+      // "tasks/1": Consulta uma tarefa
+      Tasks.findOne({where: req.params})
+        .then(result => {
+          if (result) {
+            res.json(result);
+          } else {
+            res.sendStatus(404);
+          }
+        })
+        .catch(error => {
+          res.status(412).json({msg: error.message})
+        });
+    })
+    .put((req, res) => {
+      // "tasks/1": Atualiza uma tarefa
+      Tasks.update(req.body, {where: req.params})
+        .then(res.sendStatus(204))
+        .catch(error => {
+          res.status(412).json({msg: error.message});
+        });
+    })
+    .delete((req, res) => {
+      // "tasks/1": Exclui uma tarefa
+      Tasks.destroy({where: req.params})
+        .then(result => {
+          if (result) {
+            res.sendStatus(204);
+          } else {
+            res.sendStatus(404);
+          }
+        })
+        .catch(error => {
+          res.status(412).json({msg: error.message})
+        });
+    });
 };
