3213035bad7840a6a3b68f2ee1296b96352b4d45
Bruno <bruno.silva@catho.com>
2018-11-08 09:12:49 -0200

    Revert "implementado rotas, get, get by id, post, put, delete, patch"
    This reverts commit 7a7680d831d6b75d9ae98d86824948018fae3835.

--- routes/task.js
+++ routes/task.js
@@ -1,41 +1,9 @@
 module.exports = app => {
-  const Tasks = app.db.models.Tasks;
-
-  app.route('/tasks')
-    .all((req, res, next) => {
-      //middleware de pré execução das rotas
-      //req.body.id;
-      next();
-    })
-    .get((req, res) => {
-      // "tasks": lista tarefas
-      Tasks.findAll({})
-        .then(result => res.send(result))
-        .catch(error => {
-          res.status(412).send({msg: error.menssage});
-        });
-    })
-    .post((req, res) => {
-      // "tasks": Cadastra uma nova tarefa
-      Tasks.create(req.body)
-      .then(result => res.send(result))
-      .catch(error => {
-        res.status(412).send({msg: error.menssage});
-      });
+  const Tasks = app.db.models.task;
+  app.get('/tarefas', (req, res) => {
+    Tasks.findAll({}, (task) => {
+      res.send({task});
+      res.status(200);
     });
-  app.route('/tasks/:id')
-    .all((req, res, next) => {
-      //middleware de pré execução das rotas
-      delete req.body.id;
-      next();
-    })
-    .get((req, res) => {
-      // "tasks/1": Consulta uma tarefa
-    })
-    .put((req, res) => {
-      // "tasks/1": Atualiza uma tarefa
-    })
-    .delete((req, res) => {
-      // "tasks/1": Exclui uma tarefa
-    });
+  });
 };
